3d58d128a6f57b18408897c6f5ab2544a2bc1100
pieterk <pieterklop@gmail.com>
2014-09-28 11:12:17 +0200

    Cleanup unused city-connections

--- SalesmanProblem/Route.cs
+++ SalesmanProblem/Route.cs
@@ -8,7 +8,7 @@ namespace SalesmanProblem
     {
         private readonly DistanceMatrix distanceMatrix;
         private int nCities { get { return DistanceMatrix.AMOUNT; } }
-        public City[] Cities = new City[DistanceMatrix.AMOUNT];
+        public int[] Cities = new int[DistanceMatrix.AMOUNT];
         private int total;
 
         public static Random random = new Random();
@@ -27,28 +27,11 @@ namespace SalesmanProblem
             int tot = 0;
 
             for (int i = 1; i < nCities; i++)
-                tot += distanceMatrix.CityToCity[Cities[i].Nr, Cities[i - 1].Nr];
-            tot += distanceMatrix.CityToCity[Cities[DistanceMatrix.AMOUNT - 1].Nr, Cities[0].Nr]; // complete the circle, attach end to start point
+                tot += distanceMatrix.CityToCity[Cities[i], Cities[i - 1]];
+            tot += distanceMatrix.CityToCity[Cities[DistanceMatrix.AMOUNT - 1], Cities[0]]; // complete the circle, attach end to start point
             
             total = tot;
             return tot;
-        }
-
-        /// <summary>
-        /// Generate the connections between linked cities in the route
-        /// </summary>
-        private void GenerateConnections()
-        {
-            // check if all cities occur 1 time
-            for (int i = 0; i < nCities; i++)
-            {
-                if (Cities.Count(x => x.Nr == i) != 1)
-                    throw new Exception("Invalid route");
-
-                int a = (i == 0 ? nCities - 1 : i - 1);
-                int b = (i == nCities - 1 ? 0 : i + 1);
-                Cities[i].Connections = new List<int>{Cities[a].Nr, Cities[b].Nr};
-            }
         }
 
         /// <summary>
@@ -59,37 +42,63 @@ namespace SalesmanProblem
            // randomly swap two cities
             int a = random.Next(DistanceMatrix.AMOUNT);
             int b = random.Next(DistanceMatrix.AMOUNT);
-            int temp = Cities[a].Nr;
-            Cities[a] = new City(Cities[b].Nr);
-            Cities[b] = new City(temp);
+            int temp = Cities[a];
+            Cities[a] = Cities[b];
+            Cities[b] = temp;
+            if (a == 0 || b == 0)
+                OrderRoute();
             total = 0;
+        }
+
+        /// <summary>
+        /// Order the route in that way that it starts with city=0
+        /// </summary>
+        private void OrderRoute()
+        {
+            int offset=0;
+            int n = DistanceMatrix.AMOUNT;
+            // find city=0
+            for (int i = 0; i < n; i++)
+            {
+                if (Cities[i] == 0)
+                {
+                    offset = i;
+                    break;
+                }
+            }
+
+            int[] orderedCities = new int[n];
+            // put them in order
+            for (int i = 0; i < Cities.Count(); i++)
+                orderedCities[(n - offset + i) % n] = (Cities[i]);
+
+            Cities = orderedCities;
         }
 
         public override string ToString()
         {
-            return string.Format("Total {0} route:{1}", Total(), string.Join("-", Cities.Select(x => x.Nr)));
+            return string.Format("Total {0} route:{1}", Total(), string.Join("-", Cities));
         }
 
         /// <summary>
-        /// Generate a random route
+        /// Generate a random route starts with city=0
         /// </summary>
         /// <param name="distanceMatrix"></param>
         /// <returns></returns>
         public static Route GenerateRandom(DistanceMatrix distanceMatrix)
         {
             Route route = new Route(distanceMatrix);
+            route.Cities[0] = 0;
             List<int> r = new List<int>();
-            for (int i = 0; i < DistanceMatrix.AMOUNT; i++)
+            for (int i = 1; i < DistanceMatrix.AMOUNT; i++)
                 r.Add(i);
 
-            for (int i = 0; i < DistanceMatrix.AMOUNT; i++)
+            for (int i = 1; i < DistanceMatrix.AMOUNT; i++)
             {
                 int index = random.Next(r.Count);
-                route.Cities[i] = new City(r[index]);
+                route.Cities[i] = r[index];
                 r.RemoveAt(index);
             }
-
-            route.GenerateConnections();
 
             return route;
         }
@@ -98,7 +107,7 @@ namespace SalesmanProblem
         {
             Route clone = new Route(route.distanceMatrix);
             for (int i = 0; i < DistanceMatrix.AMOUNT; i++)
-                clone.Cities[i] = new City(route.Cities[i].Nr);
+                clone.Cities[i] = route.Cities[i];
 
             return clone;
         }
